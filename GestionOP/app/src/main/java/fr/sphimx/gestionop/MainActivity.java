f9f43763bd4e98b69be231d85b2ee1acf8d8273c
Donnaint <yann.donnaint@gmail.com>
2020-05-04 02:23:45 +0200

    InitProject: AndroidStudio

---
+++ GestionOP/app/src/main/java/fr/sphimx/gestionop/MainActivity.java
@@ -0,0 +1,16 @@
+package fr.sphimx.gestionop;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.gestionop.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
